ac542b9244ee5911949ba8b94987a7b152c67a55
Jeffrey Kennedy <jeffreykennedy88@gmail.com>
2019-03-08 15:48:39 +0200

    asd

--- Database1/Database1/COAL_ChangeScript_#1_to_#2.sql
+++ Database1/Database1/COAL_ChangeScript_#1_to_#2.sql
@@ -2,7 +2,7 @@
 GO
 SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
 GO
-USE [COAL]
+USE [Utility]
 GO
 SET XACT_ABORT ON
 GO
@@ -10,27 +10,10 @@ SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
 GO
 BEGIN TRANSACTION
 GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Dropping DDL triggers'
-GO
-DROP TRIGGER [tr_DDL_BlockChangesSSMS] ON DATABASE
-GO
-DROP TRIGGER [tr_DDL_SchemaChangeLog] ON DATABASE
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Dropping [dbo].[spClearOpeningClosingBalances_Value]'
-GO
-DROP PROCEDURE [dbo].[spClearOpeningClosingBalances_Value]
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Altering [dbo].[spGetLedgerAccountTransactionDateOptions]'
-GO
+
 -- Stored Procedure
 
-ALTER procedure [dbo].[spGetLedgerAccountTransactionDateOptions]
+create procedure [dbo].[spGetLedgerAccountTransactionDateOptions]
 
 as
 
@@ -46,110 +29,6 @@ select
 	'Value Date' vchType,
 	'dteLedgerAccountTransactionValueDate' vchDateType
 
-
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Altering permissions on  [dbo].[SchemaChangeLog]'
-GO
-REVOKE DELETE ON  [dbo].[SchemaChangeLog] TO [db_user]
-GO
-REVOKE ALTER ON  [dbo].[SchemaChangeLog] TO [db_user]
-GO
-REVOKE UPDATE ON  [dbo].[SchemaChangeLog] TO [db_user]
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Altering permissions on  [dbo].[dtlLedgerAccountTransactionJournals]'
-GO
-REVOKE DELETE ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\app.bo2]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\app.bo2]
-GO
-REVOKE UPDATE ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\app.bo2]
-GO
-REVOKE DELETE ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.SQL01-PROD.ReadWriteExec]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.SQL01-PROD.ReadWriteExec]
-GO
-REVOKE UPDATE ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.SQL01-PROD.ReadWriteExec]
-GO
-REVOKE DELETE ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.SQL01-PROD.ReadWriteExecAlter]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.SQL01-PROD.ReadWriteExecAlter]
-GO
-REVOKE UPDATE ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.SQL01-PROD.ReadWriteExecAlter]
-GO
-REVOKE DELETE ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.SQL01-PROD.TempAccess]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.SQL01-PROD.TempAccess]
-GO
-REVOKE UPDATE ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.SQL01-PROD.TempAccess]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactionJournals] TO [GPP\SQL.STAGING-CPT.ReadWriteExecAlter]
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Altering permissions on  [dbo].[dtlLedgerAccountTransactions]'
-GO
-REVOKE DELETE ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\app.bo2]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\app.bo2]
-GO
-REVOKE UPDATE ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\app.bo2]
-GO
-REVOKE DELETE ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.SQL01-PROD.ReadWriteExec]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.SQL01-PROD.ReadWriteExec]
-GO
-REVOKE UPDATE ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.SQL01-PROD.ReadWriteExec]
-GO
-REVOKE DELETE ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.SQL01-PROD.ReadWriteExecAlter]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.SQL01-PROD.ReadWriteExecAlter]
-GO
-REVOKE UPDATE ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.SQL01-PROD.ReadWriteExecAlter]
-GO
-REVOKE DELETE ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.SQL01-PROD.TempAccess]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.SQL01-PROD.TempAccess]
-GO
-REVOKE UPDATE ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.SQL01-PROD.TempAccess]
-GO
-REVOKE ALTER ON  [dbo].[dtlLedgerAccountTransactions] TO [GPP\SQL.STAGING-CPT.ReadWriteExecAlter]
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Altering permissions on  [dbo].[dtlPostingsMovementToJournals]'
-GO
-REVOKE ALTER ON  [dbo].[dtlPostingsMovementToJournals] TO [GPP\SQL.STAGING-CPT.ReadWriteExecAlter]
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Altering permissions on  [dbo].[dtlPostings]'
-GO
-REVOKE ALTER ON  [dbo].[dtlPostings] TO [GPP\SQL.SQL01-PROD.ReadWriteExecAlter]
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Altering permissions on  [dbo].[dtlTransactionPointMovementsProcessed]'
-GO
-REVOKE ALTER ON  [dbo].[dtlTransactionPointMovementsProcessed] TO [GPP\SQL.STAGING-CPT.ReadWriteExecAlter]
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-PRINT N'Dropping schemas'
-GO
-DROP SCHEMA [GPP\SQL.Admin]
-GO
-DROP SCHEMA [GPP\SQL.SQL01-PROD.TempAccess]
-GO
-DROP SCHEMA [GPP\SQL.STAGING-CPT.ReadWriteExecAlter]
-GO
-COMMIT TRANSACTION
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
 -- This statement writes to the SQL Server Log so SQL Monitor can show this deployment.
 IF HAS_PERMS_BY_NAME(N'sys.xp_logevent', N'OBJECT', N'EXECUTE') = 1
 BEGIN
