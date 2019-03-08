88737dc4ae814c50be28d19f0148bc8b5e2560ad
Jeffrey Kennedy <jeffreykennedy88@gmail.com>
2019-03-08 10:23:17 +0200

    ads

--- Scripts/deploy.ps1
+++ Scripts/deploy.ps1
@@ -1,10 +1,44 @@
-       #--TeamCity checkout directory command line argument--
-		param([string]$tcDir = "")
-                #--Load the Dac.dll into the PowerShell session--
-		Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\Microsoft.SqlServer.Dac.dll"
-                #--Load the SQL Server instance--
-		$dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=(local)"
-                #--Load the DACPAC--
-		$dacPac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcDir\DeployDBDemo\bin\Release\DeployDBDemo.dacpac")
-                #--Deploy your database--
-		$dacService.deploy($dacPac, "QA", "True") 
+# Set params
+if (! $dbName)
+{
+    Write-Host "Missing required variable dbName" -ForegroundColor Yellow
+    exit 1
+}
+if (! $dbPublishProfile)
+{
+    Write-Host "Missing required variable dbPublishProfile" -ForegroundColor Yellow
+    exit 1
+}
+if (! $dbUser)
+{
+    Write-Host "Missing required variable dbUser" -ForegroundColor Yellow
+    exit 1
+}
+if (! $dbPassword)
+{
+    Write-Host "Missing required variable dbPassword" -ForegroundColor Yellow
+    exit 1
+}
+ 
+# Add the DLL
+# For 64-bit machines
+Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\Microsoft.SqlServer.Dac.dll"
+ 
+# Create the connection string
+$d = New-Object Microsoft.SqlServer.Dac.DacServices ("data source=(local);User Id = " + $dbUser + ";pwd=" + $dbPassword)
+ 
+#Load the dacpac
+$dacpac = (Get-Location).Path + "ContentDatabase.dacpac"
+$dacpacoptions = (Get-Location).Path + "Content" +  $dbPublishProfile + ".publish.xml"
+ 
+Write-Host $dacpac
+Write-Host $dacpacoptions
+ 
+#Load dacpac from file & deploy to database
+$dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($dacpac)
+ 
+#Read a publish profile XML to get the deployment options
+$dacProfile = [Microsoft.SqlServer.Dac.DacProfile]::Load($dacpacoptions)
+ 
+# Deploy the dacpac
+$d.Deploy($dp, $dbName, $true, $dacProfile.DeployOptions)
